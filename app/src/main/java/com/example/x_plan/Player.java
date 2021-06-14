dc532f6392ccbb802e86ae3ab516ba98738f0afb
dl <1830481071@qq.com>
2021-06-14 11:08:21 +0800

    信号量设置，自由选择路径

---
+++ app/src/main/java/com/example/x_plan/Player.java
@@ -0,0 +1,50 @@
+package com.example.x_plan;
+
+import android.view.View;
+import android.widget.ImageView;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class Player {
+    public ImageView player=null;
+    private boolean ifSignal;
+    private boolean[] signal=new boolean[9];//信号量
+    private Map<String,String>[] instructions=new Map[3];
+    private String[] ins_text={"","",""};
+    public List<View> views=new ArrayList<>();
+//    public List<Long> time=new ArrayList<>();
+
+    public void setIfSignal(boolean ifSignal){
+        this.ifSignal=ifSignal;
+    }
+    public void setInstructions(Map<String,String> map,int i){
+        instructions[i]=map;
+    }
+
+    public void setSignal(boolean signal,int i){
+        this.signal[i]=signal;
+    }
+
+    public void setIns_text(String text,int i){
+        ins_text[i]=text;
+    }
+
+    public boolean getIfSignal(){
+        return ifSignal;
+    }
+    public Map<String,String> getInstructions(int i){
+        return instructions[i];
+    }
+
+    public boolean getSignal(int i){
+        return signal[i];
+    }
+
+    public String getIns_text(int i){
+        return ins_text[i];
+    }
+
+
+}
